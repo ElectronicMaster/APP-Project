611696ba534790968fdfdc768243a24b176d7969
Saianuush <electronicmaster75@gmail.com>
2023-10-24 10:53:40 +0530

    24-10-2023

--- Java/Conn.java
+++ Java/Conn.java
@@ -1,14 +1,3 @@
-/*
-
-Steps to connect
-1 - Register the Driver Class(MySQL Driver Class Registration)
-2 - Creating Connection String
-3 - Creating Statement
-4 - Executing MySQL Queries
-5 - Closing the Connection
-
-*/
-
 import java.sql.*;
 import java.sql.Connection;
 
@@ -17,7 +6,6 @@ public class Conn{
     Statement s;
     Conn(){
         try{
-            // Class.forName("com.mysql.cj.jdbc.Driver");
             Class.forName("com.mysql.jdbc.Driver");
             c = DriverManager.getConnection("jdbc:mysql:///universitymanagementsystem", "root", "saianuush#3273");
             s = c.createStatement();
